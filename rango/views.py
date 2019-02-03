674c0d907cb7155657780f32575357ca382cb9bd
Gemma McDonald <2306631m@student.gla.ac.uk>
2019-02-03 16:40:12 +0000

    Chapter 7-error fixing

--- rango/views.py
+++ rango/views.py
@@ -63,8 +63,8 @@ def add_page(request, category_name_slug):
 				page.views = 0
 				page.save()
 				return show_category(request, category_name_slug)
-			else:
-				print(form.errors)
+		else:
+			print(form.errors)
 	context_dict = {'form':form, 'category': category}
 	return render(request, 'rango/add_page.html', context_dict)
 	
